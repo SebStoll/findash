c3b2a9d0c6a8664d337ab935cf9322e4343794e1
Sebastian Stoll <sebastian.stoll@immo-atlas.com>
2021-01-16 16:54:41 +0100

    new layout and visual

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 from datetime import timedelta
 import pandas as pd
@@ -13,7 +14,13 @@ from dash.dependencies import Input, Output
 # INITIALIZE APP
 # ==============
 
-app = dash.Dash(__name__)
+# Bootswatch themes:
+# CERULEAN, COSMO, CYBORG, DARKLY, FLATLY, JOURNAL, LITERA, LUMEN,
+# LUX, MATERIA, MINTY, PULSE, SANDSTONE, SIMPLEX, SKETCHY, SLATE,
+# SOLAR, SPACELAB, SUPERHERO, UNITED, YETI
+
+# app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 app.config.suppress_callback_exceptions = True
 
 
@@ -45,11 +52,7 @@ def get_options(list_stocks):
 
     return dict_list
 
-
-value_list = list(df.columns)
-value_list = [e for e in value_list if e not in ('date', 'ticker')]
-INITIAL_STOCKS_SC = [get_options(df['ticker'].unique())[0]['value']]
-INITIAL_VALUE_SC = get_options(value_list)[0]['value']
+INITIAL_STOCK_SC = get_options(df['ticker'].unique())[0]['value']
 
 dropdown_timerange = dcc.Dropdown(
     id='timerangeselector',
@@ -69,32 +72,31 @@ dropdown_timerange = dcc.Dropdown(
 dropdown_stocks = dcc.Dropdown(
     id='stockselector',
     options=get_options(df['ticker'].unique()),
-    multi=True,
-    value=INITIAL_STOCKS_SC,
+    multi=False,
+    value=INITIAL_STOCK_SC,
     # style={'backgroundColor': '#1E1E1E'},
     className='stockselector'
 )
 
-dropdown_values = dcc.Dropdown(
-    id='valueselector',
-    options=get_options(value_list),
-    multi=False,
-    value=INITIAL_VALUE_SC,
-    # style={'backgroundColor': '#1E1E1E'},
-    className='valueselector'
+timerangeselector = dbc.FormGroup(
+    [
+        dbc.Label("Select time range", html_for="timerangeselector"),
+        dropdown_timerange,
+    ]
 )
-
-graph_stocks_over_time = dcc.Graph(
-    id='stockscomparison',
-    config={'displayModeBar': False},
-    animate=True
+stockselector = dbc.FormGroup(
+    [
+        dbc.Label("Select stock", html_for="stockselector"),
+        dropdown_stocks,
+    ]
 )
+form = dbc.Form([timerangeselector, stockselector])
 
 
 # INTERACTION
 # ===========
 
-def create_df_sub(dft, selected_dropdown_stocks, selected_dropdown_value, selected_dropdown_timerange):
+def create_df_sub(dft, selected_dropdown_stock, selected_dropdown_timerange):
     df_sub = dft.copy()
 
     # xrange
@@ -125,61 +127,40 @@ def create_df_sub(dft, selected_dropdown_stocks, selected_dropdown_value, select
             df_sub.loc[df_sub.ticker == ticker, 'Close'] / close_min*100
     ##########
 
-    # check if list
-    if not isinstance(selected_dropdown_stocks, list):
-        selected_dropdown_stocks = [selected_dropdown_stocks]
-
-    # yrange
-    st = df_sub[
-        df_sub['ticker'].isin(selected_dropdown_stocks)
-    ][selected_dropdown_value]
-
-    y_min = st.min()
-    y_max = st.max()
-    y_range = y_max - y_min
-
-    y_min = y_min - 0.10*y_range
-    y_max = y_max + 0.10*y_range
-    return df_sub, x_min, x_max, y_min, y_max
+    return df_sub
 
 
 @app.callback(
     Output('stockscomparison', 'figure'),
     [
         Input('stockselector', 'value'),
-        Input('valueselector', 'value'),
         Input('timerangeselector', 'value'),
     ]
 )
-def update_stocks_comparison(selected_dropdown_stocks, selected_dropdown_value, selected_dropdown_timerange):
-    df_sub, x_min, x_max, y_min, y_max = create_df_sub(df, selected_dropdown_stocks,
-                                                       selected_dropdown_value, selected_dropdown_timerange)
+def update_stocks_comparison(selected_dropdown_stock, selected_dropdown_timerange):
+    df_sub = create_df_sub(
+        df, selected_dropdown_stock, selected_dropdown_timerange
+    )
+    data = [
+        go.Ohlc(
+            x=df_sub[df_sub['ticker'] == selected_dropdown_stock].index,
+            open=df_sub[df_sub['ticker'] == selected_dropdown_stock]['Open'],
+            high=df_sub[df_sub['ticker'] == selected_dropdown_stock]['High'],
+            low=df_sub[df_sub['ticker'] == selected_dropdown_stock]['Low'],
+            close=df_sub[df_sub['ticker'] == selected_dropdown_stock]['Close'],
+        )
+    ]
 
-    trace1 = []
-    for stock in selected_dropdown_stocks:
-        trace1.append(go.Scatter(x=df_sub[df_sub['ticker'] == stock].index,
-                                 y=df_sub[df_sub['ticker'] == stock][selected_dropdown_value],
-                                 mode='lines',
-                                 opacity=0.7,
-                                 name=stock,
-                                 textposition='bottom center'))
-
-    traces = [trace1]
-    data = [val for sublist in traces for val in sublist]
     figure = {'data': data,
               'layout': go.Layout(
-                  colorway=['#2069e0', '#FF4F00', '#29c7ac', '#ffcc00', '#26beff', '#cd4528'],
-                  template='plotly_dark',
-                  paper_bgcolor='rgba(0, 0, 0, 0)',
-                  plot_bgcolor='rgba(0, 0, 0, 0)',
-                  margin={'b': 15},
-                  hovermode='x',
+                  # hovermode='x',
                   # hoverlabel={'font': {'color': '#eae7af'}},
                   # hoverlabel={'bgcolor': '#eae7af'},
-                  autosize=True,
-                  title={'text': 'Stocks comparison', 'font': {'color': 'white'}, 'x': 0.5},
-                  xaxis={'range': [x_min, x_max]},
-                  yaxis={'range': [y_min, y_max]},
+                  # autosize=True,
+                  title={'text': selected_dropdown_stock},
+                  xaxis_rangeslider_visible=False,
+                  # xaxis={'range': [x_min, x_max]},
+                  # yaxis={'range': [y_min, y_max]},
               ),
 
               }
@@ -190,56 +171,20 @@ def update_stocks_comparison(selected_dropdown_stocks, selected_dropdown_value, 
 # APP LAYOUT
 # ==========
 
-app.layout = html.Div(
-    children=[
-        html.Div(
-            className='row',
-            children=[
-                # Left Panel Div
-                html.Div(
-                    className='four columns div-user-controls',
-                    children=[
-                        html.H2('DASH - STOCK PRICES'),
-                        html.H3('Select time range.'),
-                        html.Div(
-                            className='div-for-dropdown',
-                            children=[
-                                dropdown_timerange,
-                            ],
-                            style={'color': '#1E1E1E'}
-                        ),
-                        # Stocks comparison
-                        html.H3('Stocks comparison.'),
-                        html.Div(
-                            className='div-for-dropdown',
-                            children=[
-                                dropdown_stocks,
-                            ],
-                            style={'color': '#1E1E1E'}
-                        ),
-                        html.Div(
-                            className='div-for-dropdown',
-                            children=[
-                                dropdown_values,
-                            ],
-                            style={'color': '#1E1E1E'}
-                        ),
-                    ]
-                ),
-                # Right Panel Div
-                html.Div(
-                    className='eight columns div-for-charts',
-                    children=[
-                        graph_stocks_over_time,
-                    ]
-                )
-            ]
-        ),
-    ]
-)
+
+app.layout = dbc.Container(fluid=True, children=[
+    ## Top
+    html.H1("Bootstrap Grid System Example")
+    , form
+    ## Body
+    , dbc.Row(dbc.Col(dcc.Graph(id='stockscomparison')))
+])
 
 
 if __name__ == '__main__':
     # app.run_server(debug=True)
-    app.run_server(host='0.0.0.0', port=8050, debug=True)  # If run in docker
-    # app.run_server(host='127.0.0.1', port=5000, debug=True)  # If local run in windows
+    # app.run_server(host='0.0.0.0', port=8050, debug=True)  # If run in docker
+    app.run_server(host='127.0.0.1', port=5000, debug=True)  # If local run in windows
+
+
+
